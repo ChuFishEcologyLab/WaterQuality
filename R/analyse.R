4bf190f5e3fc80041f3a6a9b05c072a3371add79
Kevin Cazelles <kevin.cazelles@insileco.io>
2026-01-22 10:36:25 -0500

    Scale explanatory variables

--- R/analyse.R
+++ R/analyse.R
@@ -42,11 +42,27 @@ run_analysis <- function(prepare_data = FALSE) {
     df_wq <- wq_prepare_data("master_data")
   }
 
+  df_wq  <- df_wq  |>
+    dplyr::mutate(
+      hirsh_pearson_lvl7_scaled = scale(hirsh_pearson_lvl7),
+      theobald_lvl7_scaled = scale(theobald_lvl7),
+      hirsh_pearson_lvl12_scaled = scale(hirsh_pearson_lvl12),
+      theobald_lvl12_scaled = scale(theobald_lvl12)
+    )
+
 
   cli::cli_h1("Running GLMs")
   ls_res <- list()
   chem_vars <- unique(df_wq$wc_variable)
-  expl_vars <- c("hirsh_pearson_lvl7", "theobald_lvl7", "hirsh_pearson_lvl12", "theobald_lvl12")
+  expl_vars <- paste0(
+    c(
+      "hirsh_pearson_lvl7", 
+      "theobald_lvl7", 
+      "hirsh_pearson_lvl12", 
+      "theobald_lvl12"
+    ), 
+    "_scaled"
+  )
   resp_vars <- c("thr_day", "thr_month", "thr_year")
 
   out <- expand.grid(
@@ -56,13 +72,14 @@ run_analysis <- function(prepare_data = FALSE) {
   )
 
   out$effect <- 0
-  out$pval <- 0
+  out$pval  <-  out$pval_shape <- 0 
+  out$pval_signif <- FALSE
+  out$conf_low <- out$conf_high <- 0
   out$expl_dev <- 0
 
   cli::cli_progress_bar("GLMs", total = length(chem_vars))
   l <- 0
   for (i in chem_vars) {
-    cli::cli_progress_update()
     df_tmp <- df_wq |>
       dplyr::filter(wc_variable == {{ i }})
     for (j in expl_vars) {
@@ -77,9 +94,15 @@ run_analysis <- function(prepare_data = FALSE) {
         sum_mod <- summary(mod)
         out$effect[l] <- stats::coef(sum_mod)[2, 1]
         out$pval[l] <- stats::coef(sum_mod)[2, 4]
+        out$pval_signif[l] <- out$pval[l] < 1e-3
+        out$pval_shape[l] <- 19 + out$pval_signif[l] * 2
+        suppressMessages(conf_int  <- stats::confint(mod))
+        out$conf_low[l] <- conf_int[2, 1]
+        out$conf_high[l] <- conf_int[2, 2]
         out$expl_dev[l] <- (sum_mod$null.deviance - sum_mod$deviance) / sum_mod$null.deviance
       }
     }
+    cli::cli_progress_update()
   }
 
   # browser()
@@ -89,17 +112,22 @@ run_analysis <- function(prepare_data = FALSE) {
   #     out$effect[out$response_var == "hirsh_pearson_lvl12"]
   #   )
 
+  pd  <- position_dodge(width = 0.6)
   for (i in resp_vars) {
     out |>
       dplyr::filter(response_var == i) |>
-      ggplot(aes(x = effect, y = stressor, color = explainatory_var)) +
+      dplyr::arrange(
+        stressor, explanatory_var
+      ) |>
+      ggplot(aes(x = effect, y = stressor, color = explanatory_var)) +
       geom_vline(xintercept = 0, linetype = "dashed", color = "grey50") +
-      # geom_errorbar(
-      #   aes(xmin = conf_low, xmax = conf_high),
-      #   height = 0.2,
-      #   position = pd
-      # ) +
-      geom_point(position = position_dodge(width = 0.6), size = 2) +
+      geom_errorbar(
+        aes(xmin = conf_low, xmax = conf_high),
+        height = 0.2,
+        position = pd
+      ) +
+      scale_shape_manual(values = c(21, 20)) +
+      geom_point(aes(shape = pval_signif), position = pd, size = 2) +
       labs(
         x = "Effect size",
         y = NULL,
